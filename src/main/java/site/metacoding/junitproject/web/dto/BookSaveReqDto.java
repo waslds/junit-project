e2b3103834bcc8bf0634af2b24811ba57ab3f8e4
LEE, Dongsoo <waslds@daum.net>
2022-08-26 10:08:26 +0900

    책등록_test 완료

---
+++ src/main/java/site/metacoding/junitproject/web/dto/BookSaveReqDto.java
@@ -0,0 +1,18 @@
+package site.metacoding.junitproject.web.dto;
+
+import lombok.Setter;
+import site.metacoding.junitproject.domain.Book;
+
+@Setter
+public class BookSaveReqDto {
+	
+	private String title;
+	private String author;
+	
+	public Book toEntity() {
+		return Book.builder()
+				.title(title)
+				.author(author)
+				.build();
+	}
+}
