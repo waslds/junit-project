a147f90a997b47b0e063c686ccb3d320d13b0b06
LEE, Dongsoo <waslds@daum.net>
2022-08-31 10:38:38 +0900

    책 삭제하기 테스트 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -92,4 +92,37 @@ public class BookRepositoryTest {
 	}
 	
 	// 5. 책 수정
+	@Sql("classpath:db/tableInit.sql")
+	@Test
+	public void 책수정_test() {
+		// given
+		Long id = 1L;
+		String title = "junit5";
+		String author = "메타코딩";
+		Book book = new Book(id, title, author);
+		
+//		bookRepository.findAll().stream()
+//			.forEach(b -> {
+//				System.out.println(b.getId());
+//				System.out.println(b.getTitle());
+//				System.out.println(b.getAuthor());
+//				System.out.println("===================="); 
+//			});
+		 
+		// when (테스트 실행)
+		Book bookPS = bookRepository.save(book);
+		
+//		bookRepository.findAll().stream()
+//			.forEach(b -> {
+//				System.out.println(b.getId());
+//				System.out.println(b.getTitle());
+//				System.out.println(b.getAuthor());
+//				System.out.println("====================");
+//			});
+		
+		// then
+		assertEquals(id, bookPS.getId());
+		assertEquals(title, bookPS.getTitle());
+		assertEquals(author, bookPS.getAuthor());
+	}
 }
